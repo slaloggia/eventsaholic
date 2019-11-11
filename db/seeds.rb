9da641de190100ded8f683330701c64be75a12de
Daniel Ionita <danielionita@Daniels-MacBook-Pro.local>
2019-11-11 16:17:15 -0600

    added seeds for client and venue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+Client.create(username: Faker::Twitter.screen_name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+15.times do
+Venue.create(name: Faker::Restaurant.name, capacity: Faker::Number.between(from: 20, to: 300))
+end
