a0cafd3bbfcc49c7c41116048168c05fca5c0ac2
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-11-11 16:27:12 -0600

    events controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,4 @@
 class ClientsController < ApplicationController
 
-    def :
+    
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,43 @@
 class EventsController < ApplicationController
+before_action :set_event, only: [:show, :edit, :update]
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            redirect_to @event
+        else
+            # flash error messages?
+            redirect_to new_event_path
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @event.update(event_params)
+            redirect_to @event
+        else
+            # flash error messages?
+            redirect_to edit_event_path
+        end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:client_id, :venue_id, :title, :type, :date, :approved)
+    end
+
+    def set_event
+        @event = Event.find(params[:id])
+    end
+
 end
