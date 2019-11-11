e101e5be4cb11d6dcf1600da1b82a6f917d0eb3c
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-11-11 15:23:39 -0600

    created associations and deliverables doc

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :events
   resources :venues
   resources :clients
+  resources :vendors, only: [:index, :show]
+  resources :reviews, only: [:create]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
