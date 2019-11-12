450f61fde820a637e6053bae687622a7763e5581
NaveedG <nganjani@protonmail.com>
2019-11-12 11:18:01 -0600

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  root 'application#home'
   resources :events
   resources :venues
   resources :clients
   resources :vendors, only: [:index, :show]
   resources :reviews, only: [:create]
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
