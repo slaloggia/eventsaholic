0674f4ac6fd1b3f3dff1f3935f2124161a83dc30
Daniel Ionita <danielionita@Daniels-MacBook-Pro.local>
2019-11-12 11:23:38 -0600

    seeded db with venues

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,14 @@ require 'faker'
 Client.create(username: Faker::Twitter.screen_name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 end
 
-15.times do
-Venue.create(name: Faker::Restaurant.name, capacity: Faker::Number.between(from: 20, to: 300))
-end
+Venue.create(name: "Logan Bar", location:'Logan Square' , capacity: 100 , type: "Bar")
+Venue.create(name: "The Boiler Room" , location: "Logan Square" , capacity: 140 , type: "Restaurant" )
+Venue.create(name: "East Room", location: "Logan Square"  , capacity: 120 , type:"Bar" )
+Venue.create(name: "Fado Irish Pub", location: "River North" , capacity: 210, type:"Restaurant" )
+Venue.create(name: "RPM Italian", location: "River North"  , capacity: 240 , type: "Restaurant")
+Venue.create(name: 'Beatrix', location: "River North" , capacity: 155 , type: "Restaurant" )
+Venue.create(name: 'Underground', location: 'River North' , capacity: 270 , type: "Club" )
+Venue.create(name: 'Disco' , location:"River North"  , capacity: 310 , type:"Club" )
+Venue.create(name: "Au Cheval", location: "West Side" , capacity: 260, type: "Restaurant" )
+Venue.create(name: 'The Aviary', location: "West Side" , capacity: 150, type: "Bar")
+Venue.create(name: "Formento's", location: "West Side" , capacity: 270, type: "Restaurant")
