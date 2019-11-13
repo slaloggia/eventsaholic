1cd652e876a4b4b7c1dca925d0359e512140c3bf
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-11-13 13:22:33 -0600

    add vendors form works for a single vendor

--- app/controllers/event_vendors_controller.rb
+++ app/controllers/event_vendors_controller.rb
@@ -1,13 +1,13 @@
 class EventVendorsController < ApplicationController
 
     def create
-        @event_vendor = EventVendor.new
-        raise params.inspect
+        @event_vendor = EventVendor.create(event_vendor_params)
+        redirect_to show_events_path(params[:event_id])
     end
 
     private
 
     def event_vendor_params
-        params.require(:event_vendor).permit(:vendors[:vendor_id])
+        params.require(:event_vendor).permit(:vendor_id, :event_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
-  post 'event_vendor', to: 'event_vendors#create'
   resources :events
   resources :venues
   resources :clients
   resources :vendors, only: [:index, :show]
   resources :reviews, only: [:create]
+  resources :event_vendors, only: [:create]
 end
 
