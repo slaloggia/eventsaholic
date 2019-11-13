8cd91fbadc9ee0a1d8517d2b8275693893dcfbb5
NaveedG <nganjani@protonmail.com>
2019-11-13 15:07:02 -0600

    validations and flash messages

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,5 +2,9 @@ class Client < ApplicationRecord
     has_many :events
     has_many :venues, through: :events
     has_many :vendors, through: :events
+
     has_secure_password
+
+    validates_presence_of :first_name, :last_name, :username, :password, :password_confirmation
+    validates :username, uniqueness: true
 end
