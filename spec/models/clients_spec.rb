5cac1c5950360020f3031f6abe1082c4819a490a
NaveedG <nganjani@protonmail.com>
2019-11-15 10:01:17 -0600

    added client method tests

--- spec/models/clients_spec.rb
+++ spec/models/clients_spec.rb
@@ -47,5 +47,31 @@ describe Client do
     expect(Client.new(existing_username)).to be_invalid
   end
 
+  context '#upcoming_events' do
+    it 'can return all current and upcoming events of a client' do
+      new_client = Client.new(first_name: 'first name', last_name: 'last name', username: 'new client', password: 'password', password_confirmation: 'password')
+      new_client.save
+      new_venue = Venue.new(name: 'vanue')
+      new_venue.save
+      future_event = Event.new(title: 'new event', date: Date.new(2020, 1, 1), venue_id: 1, client_id: new_client.id)
+      future_event.save
+
+      expect(new_client.upcoming_events).to include(future_event)
+    end
+  end
+
+  context '#past_events' do
+    it 'can return all past events of a client' do
+      new_client = Client.new(first_name: 'first name', last_name: 'last name', username: 'new client', password: 'password', password_confirmation: 'password')
+      new_client.save
+      new_venue = Venue.new(name: 'vanue')
+      new_venue.save
+      past_event = Event.new(title: 'new event', date: Date.new(2010, 1, 1), venue_id: 1, client_id: new_client.id)
+      past_event.save
+      
+      expect(new_client.past_events).to include(past_event)
+    end
+  end
+
 end
 
