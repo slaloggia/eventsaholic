e13a4da3892c4eb28ce8132777bc977857b0740d
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-02-26 15:06:10 -0600

    Add tests for VendorsController
    - Add test for response to GET index
    - Add test for response to GET show
    - Add render_views and Capybara to config block in rails_helper

--- spec/controllers/vendors_controller_spec.rb
+++ spec/controllers/vendors_controller_spec.rb
@@ -1,1 +1,20 @@
 require 'rails_helper'
+
+describe VendorsController do
+    describe "GET vendors#index" do
+        before do
+            @vendors = FactoryBot.create_list(:random_vendor, 10)
+            visit vendors_path
+        end
+
+        it { expect(response).to have_http_status(:ok) }
+    end
+
+    describe "GET vendors#show" do
+        before do
+            @vendor = FactoryBot.create(:random_vendor)
+            visit vendor_path(@vendor.id)
+        end
+        it { expect(response).to have_http_status(:ok) }
+    end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -7,6 +8,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -33,6 +35,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.render_views
+  config.include Capybara::DSL
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
